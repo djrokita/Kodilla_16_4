2b2f536a90c68e685eb6262a0f5b389875934642
Krzych <krzych.pierzchala@gmail.com>
2017-12-06 13:25:34 +0100

    Installed Optimize-js-plugin

--- webpack.config.js
+++ webpack.config.js
@@ -1,33 +1,40 @@
-const path = require('path');
-const webpack = require('webpack');
-const HTMLWebpackPlugin = require('html-webpack-plugin');
-const UglifyJSPlugin = require('uglifyjs-webpack-plugin');
+const path = require("path");
+const webpack = require("webpack");
+const HTMLWebpackPlugin = require("html-webpack-plugin");
+const UglifyJSPlugin = require("uglifyjs-webpack-plugin");
+const OptimizeJsPlugin = require("optimize-js-plugin");
 
 module.exports = {
-  entry: './src/index.js',
+  entry: "./src/index.js",
   output: {
-    path: path.resolve(__dirname, 'build'),
-    filename: 'app.bundle.js'
+    path: path.resolve(__dirname, "build"),
+    filename: "app.bundle.js"
   },
-  plugins: [new HTMLWebpackPlugin({
-    template: 'src/index.html',
-    filename: 'index.html',
-    inject: 'body'
+  plugins: [
+    new HTMLWebpackPlugin({
+      template: "src/index.html",
+      filename: "index.html",
+      inject: "body"
     }),
-    new webpack.optimize.UglifyJsPlugin()
+    new webpack.optimize.UglifyJsPlugin(),
+    new OptimizeJsPlugin({
+      sourceMap: false
+    })
   ],
   module: {
-    rules: [{
+    rules: [
+      {
         test: /\.js$/,
         loader: "babel-loader"
       },
       {
         test: /\.css$/,
-        use: [{
-            loader: 'style-loader'
+        use: [
+          {
+            loader: "style-loader"
           },
           {
-            loader: 'css-loader',
+            loader: "css-loader",
             options: {
               modules: true
             }

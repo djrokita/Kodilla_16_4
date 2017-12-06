f40e9a6a664cd025a16b5258630eb9eac1534081
Krzych <krzych.pierzchala@gmail.com>
2017-12-06 12:16:00 +0100

    Installed html-webpack-plugin

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,5 @@
 const path = require('path');
+const HTMLWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
   entry: './src/index.js',
@@ -6,6 +7,11 @@ module.exports = {
     path: path.resolve(__dirname, 'build'),
     filename: 'app.bundle.js'
   },
+  plugins: [new HTMLWebpackPlugin({
+    template: 'src/index.html',
+    filename: 'index.html',
+    inject: 'body'
+  })],
   module: {
     rules: [{
         test: /\.js$/,
